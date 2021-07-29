87570d8a6b09fd1d8ceda7970bcba77a2724bbe0
LRiosB <36747760+LRiosB@users.noreply.github.com>
2021-07-28 23:44:09 -0300

    Add files via upload

---
+++ teste2.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS teste2 (
+    `arroz_feijao_batata` VARCHAR(5) CHARACTER SET utf8
+);
+INSERT INTO teste2 VALUES
+    ('0;0;0'),
+    ('0;0;1'),
+    ('0;1;0'),
+    ('0;1;1'),
+    ('1;0;0'),
+    ('1;0;1'),
+    ('1;1;0'),
+    ('1;1;1');
